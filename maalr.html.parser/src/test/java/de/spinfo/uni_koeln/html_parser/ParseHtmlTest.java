67e894b083cf6c81481508000d537d95671204f1
fmondaca <f.mondaca@uni-koeln.de>
2015-08-11 14:19:01 +0200

    html parsing up

--- maalr.html.parser/src/test/java/de/spinfo/uni_koeln/html_parser/ParseHtmlTest.java
+++ maalr.html.parser/src/test/java/de/spinfo/uni_koeln/html_parser/ParseHtmlTest.java
@@ -1,8 +1,10 @@
 package de.spinfo.uni_koeln.html_parser;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.junit.Test;
 
@@ -14,33 +16,54 @@ public class ParseHtmlTest {
 
 	ParseHtml parser = new ParseHtml();
 
-	// @Ignore
-	@Test
-	public void testAddEntryTags() throws IOException {
+	// WORKFLOW
 
-		parser.addEntryTags(ParseHtml.input_dir + ParseHtml.doc);
+	// 1. modifyTags
+	// 2. addEntryTags
+	// 3. identateEntries
+
+	@Test
+	public void doOneAndTwo() throws IOException {
+
+		List<String> modified = parser.modifyTags(ParseHtml.input_dir
+				+ ParseHtml.doc);
+		List<String> markedentries = parser.addEntryTagsfromList(modified);
+
+		FileUtils.printList(markedentries, ParseHtml.input_dir,
+				"markedEntries", "html");
 
 	}
+
+	// THREE
 
 	// @Ignore
 	@Test
 	public void testIdentateEntries() throws IOException {
 
-		List<RawEntry> rawEntries = parser.identateEntries(ParseHtml.input_dir
-				+ ParseHtml.markedLemmata);
-		
-		
-		for (RawEntry e : rawEntries){
-			
-			if (e.getPages()== null){
-				
+		File f = new File(ParseHtml.input_dir + ParseHtml.markedLemmata);
+
+		List<RawEntry> rawEntries = parser.identateEntries(f);
+
+		for (RawEntry e : rawEntries) {
+
+			if (e.getPages() == null) {
+
 				System.out.println(e.getLemma());
 			}
 		}
 
-		FileUtils.printList(rawEntries, ParseHtml.output_dir, "rawEntries_");
+		FileUtils.printList(rawEntries, ParseHtml.output_dir, "rawEntries_",
+				"txt");
 
 		// List<Entry> entries = parser.getEntries(rawEntries);
+
+	}
+
+	// @Ignore
+	@Test
+	public void testAddEntryTags() throws IOException {
+
+		parser.addEntryTags(ParseHtml.input_dir + ParseHtml.doc);
 
 	}
 
@@ -52,6 +75,31 @@ public class ParseHtmlTest {
 						+ "20150898_mappedentries.txt");
 
 		FileUtils.printMap(map, ParseHtml.output_dir, "mapping");
+
+	}
+
+	@Test
+	public void modifyTagsTest() throws IOException {
+
+		parser.modifyTags(ParseHtml.input_dir + ParseHtml.doc);
+
+		// FileUtils.printMap(map, ParseHtml.output_dir, "mapping");
+
+	}
+
+	@Test
+	public void getAllFontTypes() throws IOException {
+
+		Set<String> myset = parser.getAllFontTypes(ParseHtml.input_dir
+				+ ParseHtml.doc);
+
+		for (String s : myset) {
+
+			System.out.println(s);
+
+		}
+
+		// FileUtils.printMap(map, ParseHtml.output_dir, "mapping");
 
 	}
 
